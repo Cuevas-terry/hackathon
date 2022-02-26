05ab345c495ddf5ea5ab30a352de88ff1e690184
GabrielaC <gcc67@scarletmail.rutgers.edu>
2022-02-26 09:37:22 -0500

    first commit

---
+++ webApp/background.js
@@ -0,0 +1,7 @@
+
+let color = '#ede7db';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
